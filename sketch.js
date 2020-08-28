960f2bcda5797ea3c91e304752316b4111435733
ZionRyder <69284607+ZionRyder@users.noreply.github.com>
2020-08-28 10:15:02 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+var packageBody,ground;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+var box1, box2, box3;
+
+
+
 
 function preload()
 {
@@ -15,7 +19,12 @@ function setup() {
 	createCanvas(800, 700);
 	rectMode(CENTER);
 	
-
+	box1Sprite=createSprite(0,0,200,20);
+	box1Sprite.shapeColor="red"
+	box2Sprite=createSprite(0,0,20,100);
+	box2Sprite.shapeColor="red"
+	box3Sprite=createSprite(0,0,20,100);
+	box3Sprite.shapeColor="red"
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
@@ -39,6 +48,13 @@ function setup() {
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
 
+	
+	box1 = Bodies.rectangle(400, 630, 2000, 20 , {isStatic:true} );
+	World.add(world, box1);
+	box2 = Bodies.rectangle(width/1.63, 600, 20, 100 , {isStatic:true} );
+	World.add(world, box2);
+	box3 = Bodies.rectangle(width/2.6, 600, 20, 100 , {isStatic:true} );
+	World.add(world, box3);
 
 	Engine.run(engine);
   
@@ -48,10 +64,18 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
+box1.debug
+box2.debug
+box3.debug
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
+  box1Sprite.x= box1.position.x
+  box1Sprite.y= box1.position.y + 20
+  box2Sprite.x= box2.position.x 
+  box2Sprite.y= box2.position.y 
+  box3Sprite.x= box3.position.x 
+  box3Sprite.y= box3.position.y  
   drawSprites();
- 
 }
 
 function keyPressed() {
